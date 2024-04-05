cfe4280633a1c2d4df0e6d2ebd0afd4d82347195
Tanu Kumari <tannukumari21@navgurukul.org>
2024-04-06 00:56:46 +0530

    without class

--- app/search_bar.js
+++ app/search_bar.js
@@ -1,131 +1,52 @@
-// const SearchBar = () => {
-//   return (
-//   <>
-//     <div className="search">
-//         <input placeholder="Search" className="search-bar" type="search" />
-//         <button className="go_button ">Go</button>
-//     </div>
-//   </>
-//   )
-// }
-// export default SearchBar;
-
-import { useState } from 'react';
+import React, { useState } from "react";
 
 const SearchBar = () => {
-  const [searchInput, setSearchInput] = useState('');
+  const [userInput, setUserInput] = useState("");
   const [newsData, setNewsData] = useState([]);
 
-  const handleSearchInputChange = (e) => {
-    setSearchInput(e.target.value);
+  const handlesearchInput = (e) => {
+    setUserInput(e.target.value);
   };
 
-  const handleSearchButtonClick = () => {
-    console.log('Search clicked:', searchInput);
-    fetchNews(searchInput);
+  const handlegoButton = () => {
+    console.log("search button clicked", userInput);
+    fetchNews(userInput);
   };
 
-  class Product{
-    constructor(title,description,urlToImage,url){
-      this.title = title;
-      this.description = description;
-      this.urlToImage=urlToImage;
-      this.url=url;
-    }
-  }
-
-  
-  const fetchNews = (searchQuery) => {
+  const fetchNews = (userInput) => {
     const apiKey = "44137023ef4d47af8275855373f32548";
-    const url = `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${apiKey}`;
-  
+    const url = `https://newsapi.org/v2/everything?q=${userInput}&apiKey=${apiKey}`;
+
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const news = data.articles.map(article => {
-          return new Product(article.description, article.title, article.url, article.urlToImage);
-        });
-        console.log(news,"NEWS");
-        setNewsData(news);
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setNewsData(data.articles);
       })
-      
-      .catch(error => {
-        console.error('Error fetching news:', error);
+
+      .catch((error) => {
+        console.log("error", error);
       });
   };
-  
-    return (
-      <>
-        <div className="search">
-          <input value={searchInput} placeholder="Search" className="search-bar" type="search" onChange={handleSearchInputChange} />
-          <button onClick={handleSearchButtonClick} className="go_button">Go</button>
-        </div>
-        {newsData.map((item) => (
+
+  return (
+    <>
+      <div className="search">
+        <input onChange={handlesearchInput} placeholder="Search" className="search-bar" type="search" />
+        <button onClick={handlegoButton} className="go_button">Go</button>
+
+        {newsData.slice(0, 9).map((item) => (
           <div key={item.description}>
             <img className='image' src={item.urlToImage}/>
-            <h1>{item.title}</h1>
+            <p>{item.title}</p>
             <p>{item.description}</p>
             <p>{item.url}</p>
           </div>
-        ))} 
-      </>
-    );
-  };
-  
-  export default SearchBar;
+        ))}
 
+      </div>
+    </>
+  );
+};
 
-
-
-// import { useState } from 'react';
-
-// const SearchBar = () => {
-//   const [searchInput, setSearchInput] = useState('');
-//   const [newsData, setNewsData] = useState([]);
-
-//   const handleSearchInputChange = (e) => {
-//     setSearchInput(e.target.value);
-//   };
-
-//   const handleSearchButtonClick = () => {
-//     console.log('Search clicked:', searchInput);
-//     fetchNews(searchInput);
-//   };
-
-//   const fetchNews = (searchQuery) => {
-//     const apiKey = "44137023ef4d47af8275855373f32548";
-//     const url = https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${apiKey};
-
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(data => {
-//         console.log(data); 
-//         setNewsData(data.articles); // Assuming data is an array of articles, set the state with fetched data
-//       })
-//       .catch(error => {
-//         console.error('Error fetching news:', error);
-//       });
-//   };
-
-//   return (
-//     <>
-//       <div className="search">
-//         <input value={searchInput} placeholder="Search" className="search-bar" type="search" onChange={handleSearchInputChange} />
-//         <button onClick={handleSearchButtonClick} className="go_button">Go</button>
-//       </div>
-//       {newsData.map((item) => (
-//         <div key={item.description}>
-//           <h1>{item.title}</h1>
-//           <p>release_date:{item.publishedAt}</p>
-//           <p>original_language:{item.source.name}</p>
-//           <img src={item.urlToImage} alt="News Thumbnail" />
-//           <p>{item.description}</p>
-//           <p>{item.url}</p>
-//         </div>
-//       ))}
-//     </>
-//   );
-// };
-
-// export default SearchBa
-
+export default SearchBar;
